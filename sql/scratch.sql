6d837bf82eb277856eaa86fcda764952cf723d37
darrahts <timothy.s.darrah@vanderbilt.edu>
2022-01-12 08:00:48 -0600

    removed

--- sql/scratch.sql
+++ sql/scratch.sql
@@ -10,6 +10,9 @@ select * from summary_tb;
 
 select * from engine_ncmapss_tb;
 
+select * from asset_tb;
+
+select * 
 
 select ent.* from (select *, row_number() over() rn from engine_ncmapss_tb) ent where ent.rn % 2 = 0;
 
@@ -22,9 +25,7 @@ select * from degradation_tb;
 
 
 
-
-
-
+select * from information_schema.tables where table_schema = 'public';
 
 
 
