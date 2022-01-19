e20afe10d3cf27e1a89bb1a0ed1ccaa4f98c63ca
darrahts <timothy.s.darrah@vanderbilt.edu>
2022-01-19 15:53:21 -0600

    [WIP] tuning hyperparameters and architecture file

---
+++ package/tuning.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+import keras_tuner as kt
+
+from kerastuner_tensorboard_logger import (
+    TensorBoardLogger,
+    setup_tb  # Optional
+)
+
+
